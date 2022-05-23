f88981c0b25662f086c67471fc22d510e86656e0
Miguel Soriano <originalsorianom@gmail.com>
2022-05-23 01:55:26 -0700

    Updated script.js code for passwordlength prompt

--- assets/script.js
+++ assets/script.js
@@ -5,10 +5,13 @@ function generatePassword(){
   var lowerletters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; 
   var upperletters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   var specialletters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', ',', '+'];
+  var passwordlength = prompt('How many character do you want in password? (Must be between 8 and 128)');
+  
+
   
 
 
-  return 
+ 
 
 }
 // Get references to the #generate element
